11bf987745daa389764d05ba2cc28e22f00ac654
PorfirioTramontana <porfirio.tramontana@gmail.com>
2018-11-02 16:50:17 +0100

    Schema

--- src/generator/Generator.java
+++ src/generator/Generator.java
@@ -12,6 +12,24 @@ public class Generator {
 	public void generate(Strategy s) {
 		// Algoritmo di generazione
 	
+		//Apri file nel quale generare i test
+		
+		//Scrivi il preambolo
+		
+		//se la strategia e' random
+			// cicla sul numero di test previsti
+				//scrivi il preambolo del singolo test
+				// cicla sulla lunghezza prevista per i test
+					//genera un evento nell'elenco di quelli proposti
+					//scrivi l'evento nel file
+				//scrivi la parte finale del test
+			//scrivi la parte finale del file di test
+		
+		//chiudi il file
+				
+	}
+	
+	public void addPreamble() {
 		
 	}
 
